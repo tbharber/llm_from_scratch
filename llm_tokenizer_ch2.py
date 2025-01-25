e69dd68b004c331e8b92ddcb7379696a92f4e3dd
tbharber <tbharber@yahoo.com>
2025-01-25 14:48:41 -0500

    update ch2

--- llm_tokenizer_ch2.py
+++ llm_tokenizer_ch2.py
@@ -187,19 +187,22 @@ print (tokenizer.decode(tokenizer.encode(text)))
 import importlib.metadata
 import tiktoken
 
+#Print version
 print("tiktoken version:", importlib.metadata.version("tiktoken"))
 
+#Initialize tokenizer
 tokenizer = tiktoken.get_encoding("gpt2")
 
+#Set sample text
 text = (
     "Hello, do you like tea? <|endoftext|> In the sunlit terraces"
      "of someunknownPlace."
 )
 
+#Encode text
 integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
-
 print(integers)
 
+#Decode text
 strings = tokenizer.decode(integers)
-
 print(strings)
